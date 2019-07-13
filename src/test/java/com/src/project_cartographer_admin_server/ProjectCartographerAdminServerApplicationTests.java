2bacb056a5fe259e7889793fd8b27e11c1a3e00f
shayaantx <shayaantx@gmail.com>
2019-07-12 20:06:30 -0400

    Fix test cases from failing by using in-memory db

--- src/test/java/com/src/project_cartographer_admin_server/ProjectCartographerAdminServerApplicationTests.java
+++ src/test/java/com/src/project_cartographer_admin_server/ProjectCartographerAdminServerApplicationTests.java
@@ -2,11 +2,16 @@ package com.src.project_cartographer_admin_server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class ProjectCartographerAdminServerApplicationTests {
 
 	@Test
