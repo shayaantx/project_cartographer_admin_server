55c1b433f375f20dae5e5cf858cab4b9da896eb0
shayaantx <shayaantx@gmail.com>
2019-12-08 16:57:41 -0500

    Update ProjectCartographerAdminServerApplicationTests.java

--- src/test/java/com/src/project_cartographer_admin_server/ProjectCartographerAdminServerApplicationTests.java
+++ src/test/java/com/src/project_cartographer_admin_server/ProjectCartographerAdminServerApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles(profiles = "test")
+@ActiveProfiles(profiles = {"test", "simple-security"})
 public class ProjectCartographerAdminServerApplicationTests {
   @Test
   public void contextLoads() {}
