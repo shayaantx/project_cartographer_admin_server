685e5ed943d3e2300cb1191493f478f2c729ffde
shayaantx <shayaantx@gmail.com>
2020-04-30 19:10:23 -0400

    Add periodic new user activation

---
+++ src/main/java/com/src/project_cartographer_admin_server/services/NewUserActivator.java
@@ -0,0 +1,21 @@
+package com.src.project_cartographer_admin_server.services;
+
+import com.src.project_cartographer_admin_server.businessobjects.NewUserBO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * Unfortunately the cartographer activation emails have been blacklist by hotmail and often get put in spam folder by other
+ * email providers. So we will activate new users periodically (every 5 minutes) instead
+ */
+@Service
+public class NewUserActivator {
+  @Scheduled(fixedDelay = 360000)
+  public void scheduleFixedDelayTask() {
+    newUserBO.activateAll();
+  }
+
+  @Autowired
+  private NewUserBO newUserBO;
+}
