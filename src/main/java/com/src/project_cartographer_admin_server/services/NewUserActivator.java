d9fa050e5d3fd8e15d7f635bdfe4d8e9f1df23ac
shayaantx <shayaantx@gmail.com>
2020-05-10 23:28:25 -0400

    Turn off new user activator

--- src/main/java/com/src/project_cartographer_admin_server/services/NewUserActivator.java
+++ src/main/java/com/src/project_cartographer_admin_server/services/NewUserActivator.java
@@ -2,6 +2,7 @@ package com.src.project_cartographer_admin_server.services;
 
 import com.src.project_cartographer_admin_server.businessobjects.NewUserBO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +19,7 @@ public class NewUserActivator {
 
   @Autowired
   private NewUserBO newUserBO;
+
+  @Value("${new.user.activator:false}")
+  private boolean activateNewUsers;
 }
